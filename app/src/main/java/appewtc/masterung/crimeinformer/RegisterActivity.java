39d7e073a5bdfab22da067b1398ae4da810524b5
masterUNG <phrombutr@gmail.com>
2015-11-27 13:00:50 +0700

    Cancel Button Finish

--- app/src/main/java/appewtc/masterung/crimeinformer/RegisterActivity.java
+++ app/src/main/java/appewtc/masterung/crimeinformer/RegisterActivity.java
@@ -2,12 +2,41 @@ package appewtc.masterung.crimeinformer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, surnameEditText, idCardEditText, phoneEditText, eMailEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Bind Widget
+        bindWidget();
+
+
+    }   // Main Method
+
+    public void clickOK(View view) {
+
     }
-}
+
+    public void clickCancel(View view) {
+        finish();
+    }
+
+
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        idCardEditText = (EditText) findViewById(R.id.editText3);
+        phoneEditText = (EditText) findViewById(R.id.editText4);
+        eMailEditText = (EditText) findViewById(R.id.editText5);
+    }
+
+}   // Main Class
