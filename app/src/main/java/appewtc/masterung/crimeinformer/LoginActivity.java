ff3934525f5dfa46a73f059b050d60005e4c49d1
masterUNG <phrombutr@gmail.com>
2015-12-25 11:14:42 +0700

    Check Space Finish

--- app/src/main/java/appewtc/masterung/crimeinformer/LoginActivity.java
+++ app/src/main/java/appewtc/masterung/crimeinformer/LoginActivity.java
@@ -1,14 +1,50 @@
 package appewtc.masterung.crimeinformer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Bind Widget
+        bindWidget();
+
     }   // Main Method
+
+    public void clickLogin(View view) {
+
+        //Check Space
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+
+            //Have Space
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.nagativeDialog(LoginActivity.this, "Have Space", "Please Fill All Every Blank");
+
+        } else {
+
+            //No Space
+
+        }
+
+    }   // clickLogin
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passwordEditText = (EditText) findViewById(R.id.editText7);
+    }
 
 }   // Main Class
